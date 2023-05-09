b6d572796bd28ee5eef6f7d5d1dea6fd453335af
dot.exe <57018141+dot-exe@users.noreply.github.com>
2023-05-09 10:54:24 +0200

    add first view

--- biblioteka/katalog/views.py
+++ biblioteka/katalog/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import Autor, Gatunek, Ksiazka, InstacjaKsiazki
+
+
+def index(request):
+    num_ks = Ksiazka.objects.all().count()
+    num_in = InstacjaKsiazki.objects().all().count()
+    num_in_d = InstacjaKsiazki.objects.filter(status_exact='d').count()
+    num_au = Autor.objects().count()
+
+    return render(
+        request,
+        'index.html',
+        context={
+            'num_ks': num_ks,
+            'num_in': num_in,
+            'num_in_d': num_in_d,
+            'num_au': num_au
+        }
+    )
